0ed4868f219e59dfd396c13ea5546c63666b5140
Alex Avery <aja9675@rit.edu>
2021-04-17 15:15:12 -0400

    Add profiling
    Signed-off-by: Alex Avery <aja9675@rit.edu>

--- track_bees.py
+++ track_bees.py
@@ -86,14 +86,24 @@ def track_bees(args):
 	input_size = bee_ds.input_size
 	ic(bee_ds.input_size)
 
+	# Vars for tracking
 	prev_pts = np.array([])
 	running_pairs = []
+
+	# Vars for profiling
+	total_exec_time_accumulator = 0
+	exec_time_accumulator = 0
+	num_exec_frames = 0
+
 	for frame_num in range(num_frames):
 
 		# Read frame from video
 		ret, frame = vid.read()
 		if frame is None:
 			break
+
+		if args.enable_timing:
+			start_time = time.time()
 
 		# Rotate if rotation is set
 		if args.rotate == -90:
@@ -117,6 +127,20 @@ def track_bees(args):
 
 		# Get prediction centroids (predicted points)
 		pred_pts = helper.get_centroids(pred)
+		num_bees = len(pred_pts)
+
+		# Since what we really care about is # bees, stop time profiling here
+		# The rest is for visualization
+		if args.enable_timing and frame_num > 0:
+			iter_time = time.time() - start_time
+			total_exec_time_accumulator += iter_time
+			exec_time_accumulator += iter_time
+			num_exec_frames += 1
+			if frame_num % int(fps) == 0:
+				avg_exec_time = exec_time_accumulator / num_exec_frames
+				ic("Avg exec time: ", avg_exec_time)
+				exec_time_accumulator = 0
+				num_exec_frames = 0
 
 		# Use bipartite graph minimum weight matching to associate detections
 		if len(pred_pts) > 0 and len(prev_pts) > 0 and not args.disable_tracking:
@@ -139,10 +163,22 @@ def track_bees(args):
 			for pred_pt in pred_pts:
 				cv2.circle(frame, tuple((pred_pt[1],pred_pt[0])), 5, VIS_COLOR, cv2.FILLED)
 
+		# Draw # of bees in text on bottom left of image
+		num_bees_text = "# Bees: %i" % num_bees
+		bottom_left = (0, frame.shape[0]-20)
+		frame = cv2.putText(frame, num_bees_text, org=bottom_left, fontFace=cv2.FONT_HERSHEY_SIMPLEX, 
+                   fontScale=1, color=(255, 0, 0), thickness=2, lineType=cv2.LINE_AA)
+
 		# Show results
 		helper.show_image("Predictions", frame, delay=frame_duration)
 
 		prev_pts = deepcopy(pred_pts)
+
+	# Calculate the average processing fps. num_frames-1 b/c we didnt' count the first frame initialization delay
+	avg_exec_time = total_exec_time_accumulator / (num_frames-1)
+	measured_fps = 1 / avg_exec_time
+	ic("Overall avg exec time: ", avg_exec_time)
+	ic("Overall FPS: ", measured_fps)
 
 
 if __name__ == "__main__":
@@ -150,12 +186,12 @@ if __name__ == "__main__":
 	parser.add_argument('--video_file_in', default='/data/datasets/bees/bee_videos/best_bee_data/20180522_173523.mp4', type=str, help='Video file to process')
 	parser.add_argument('--data_dir', default='/data/datasets/bees/ak_bees/images', type=str, help='Dataset dir')
 	parser.add_argument('--model_dir', default='./results/latest', type=str, help='results/<datetime> dir')
-	parser.add_argument('--debug', action='store_true', default=False, help='Enable debugging prints and vis')
 	parser.add_argument('--fps', type=int, default=0, help='Enable debugging prints and vis')
 	parser.add_argument('-r', '--rotate', type=int, default=0, help='Rotate [-90,90')
 	parser.add_argument('--disable_tracking', action='store_true', default=False, help='Disable tracking')
 	parser.add_argument('-t', '--threshold', type=int, default=50, help='Distance threshold for tracking')
 	parser.add_argument('--tracker_frame_len', type=int, default=15, help='Number of frames to show tracking visualization')
+	parser.add_argument('--enable_timing', action='store_true', default=False, help='Enables performance timing')
 	parser.set_defaults(func=track_bees)
 
 	args = parser.parse_args()
